ec0216073b203c2042240f4beb5b9b2a9a8c4455
Cihan <cihanturkess@gmail.com>
2021-07-15 18:38:54 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+    <Timeline
+        v-for="(note, index) in notes"
+        :key="note.id"
+        :note="note"
+        :index="index"
+        :length="notes.length"
+    />
+</template>
+
+<script>
+    import Timeline from "@/components/Timeline"
+
+    export default {
+        name: 'App',
+        components: { Timeline },
+        data() {
+            return {
+                notes: [
+                    {
+                        title: "dcode",
+                        paragraph: "My fist YouTube video was a tutorial on how to build a client-server socket app in Java.",
+                        date: "July, 23 2021"
+                    },
+                    {
+                        title: "Arin Yazılım",
+                        paragraph: "My fist YouTube video was a tutorial on how to build a client-server socket app in Java.",
+                        date: "July, 23 2021"
+                    },
+                    {
+                        title: "kablosuzkedi",
+                        paragraph: "My fist YouTube video was a tutorial on how to build a client-server socket app in Java.",
+                        date: "July, 23 2021"
+                    },
+                    {
+                        title: "Adem İlter",
+                        paragraph: "My fist YouTube video was a tutorial on how to build a client-server socket app in Java.",
+                        date: "July, 23 2021"
+                    },
+                    {
+                        title: "Prototürk",
+                        paragraph: "My fist YouTube video was a tutorial on how to build a client-server socket app in Java.",
+                        date: "July, 23 2021"
+                    },
+                    {
+                        title: "Vue Turkey",
+                        paragraph: "My fist YouTube video was a tutorial on how to build a client-server socket app in Java.",
+                        date: "July, 23 2021"
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import url("https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;500;700&display=swap");
+
+    * {
+        padding: 0;
+        margin: 0;
+        list-style: none;
+        border: none;
+        text-decoration: none;
+        box-sizing: border-box;
+        -webkit-overflow-scrolling: touch;
+        font-family: "Fira Sans", sans-serif;
+
+        &:focus {
+            outline: 0;
+        }
+    }
+    body {
+        background-color: #009578;
+        padding: 25px;
+    }
+</style>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="timeline" :class="reverse">
+        <div class="timeline__component item--1">
+            <div class="timeline__date" :class="right">
+                {{ note.date }}
+            </div>
+        </div>
+        <div class="timeline__middle item--2">
+            <div class="timeline__point"></div>
+            <div
+                v-if="length === index + 1"
+                class="timeline__point timeline__point--bottom"
+            ></div>
+        </div>
+        <div class="timeline__component timeline__component--bg item--3">
+            <h2 class="timeline__title">{{ note.title }}</h2>
+            <p class="timeline__paragraph">
+                {{ note.paragraph }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Timeline",
+        props: {
+            note: {
+                type: Object,
+                required: true
+            },
+            index: {
+                type: Number
+            },
+            length: {
+                type: Number
+            }
+        },
+        computed: {
+            reverse() {
+                return {
+                    "reverse": this.index % 2 === 1
+                }
+            },
+            right() {
+                return {
+                    "timeline__date--right": this.index % 2 === 0
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .timeline {
+        max-width: 750px;
+        margin: 0 auto;
+        display: grid;
+        grid-template-columns: 1fr 3px 1fr;
+        grid-template-areas: "item1 item2 item3";
+        color: white;
+
+        &.reverse {
+            grid-template-areas: "item3 item2 item1";
+        }
+        .item--1 {
+            grid-area: item1;
+        }
+        .item--2 {
+            grid-area: item2;
+        }
+        .item--3 {
+            grid-area: item3;
+        }
+
+        &__component {
+            margin: 0 20px 20px 20px;
+
+            &--bg {
+                padding: 1.5em;
+                background-color: rgba(255, 255, 255, 0.2);
+                border-radius: 10px;
+                box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+            }
+            .timeline__title {
+                font-size: 1.15em;
+            }
+            .timeline__paragraph {
+                margin: 1em 0;
+                line-height: 1.5;
+            }
+        }
+        &__date {
+            &--right {
+                text-align: right;
+            }
+        }
+        &__middle {
+            background: white;
+            position: relative;
+        }
+        &__point {
+            position: absolute;
+            top: 0;
+            left: 50%;
+            transform: translateX(-50%);
+            width: 15px;
+            height: 15px;
+            background-color: white;
+            border-radius: 50%;
+
+            &--bottom {
+                top: initial;
+                bottom: 0;
+            }
+        }
+    }
+</style>
